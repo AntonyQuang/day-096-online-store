355b2b4e7e3f70f0784365fdc98ef190adeeaee3
Antony Quang <antony.quang@googlemail.com>
2022-10-18 21:13:07 +0100

    Fix app init

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
 from flask import Flask, render_template, redirect, url_for, flash, abort
-from flask_sqlalchemy import SQLAlechemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user, login_manager
 from flask_gravatar import Gravatar
-from forms import CreatePostForm, RegisterForm, LogInForm, CommentForm
+from forms import CreateItemForm, RegisterForm, LogInForm
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # Creating a Flask instance
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "SecretKey"
-
+app.config["SECRET_KEY"] = "SecsadasdsaretKey"
 # Connect to the database
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("TRUE_DATABASE_URL",  "sqlite:///shop.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("TRUE_DATABASE_URL", "sqlite:///shop.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlechemy(app)
+db = SQLAlchemy(app)
 
 # Introducing login manager
-login_manger = LoginManager()
-login_manger.init_app(app)
+login_manager = LoginManager(app)
+login_manager.init_app(app)
 
-# Inserting Gravatar, a random avatar generator
-gravatar = Gravatar(app,
-                    size=100,
-                    rating='g',
-                    default='retro',
-                    force_default=False,
-                    force_lower=False,
-                    use_ssl=False,
-                    base_url=None)
 
 # Setting out tables. One for users, one for items for sale, one for current orders
+
 
 class Item(db.Model):
     __tablename__ = "items"
@@ -49,6 +40,11 @@ class User(UserMixin, db.Model):
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     password = db.Column(db.String(1000))
+    address_1 = db.Column(db.String(200), nullable=False)
+    address_2 = db.Column(db.String(200))
+    postcode = db.Column(db.String(10), nullable=False)
+    city = db.Column(db.String(200), nullable=False)
+    country = db.Column(db.String(200), nullable=False)
     orders = db.relationship('BlogPost', back_populates="user")
 
 
@@ -59,7 +55,24 @@ class Orders(db.Model):
     items = db.relationship("Item", back_populates="name")
 
 
-db.create_all()
+# As of Flask-SQLAlchemy 3.0, all access to db.engine (and db.session) requires an active Flask application context.
+# db.create_all uses db.engine, so it requires an app context.
+
+with app.app_context():
+    db.create_all()
+
+
+# Inserting Gravatar, a random avatar generator
+gravatar = Gravatar(app,
+                    size=100,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    force_lower=False,
+                    use_ssl=False,
+                    base_url=None)
+
+
 
 # what does .user_loader do? You will need to provide a user_loader callback. This callback is used to reload the
 # user object from the user ID stored in the session. It should take the str ID of a user, and return the
@@ -72,7 +85,9 @@ def load_user(user_id):
 @app.route('/')
 def home():
     items = Item.query.all()
-    return render_template("index.html", items=items, logged_in=current_user.is_authenticated, user_id=current_user.get_id())
+    return "hello"
+    return render_template("index.html", items=items, logged_in=current_user.is_authenticated,
+                           user_id=current_user.get_id())
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -81,12 +96,28 @@ def register():
 
     if register_form.validate_on_submit():
         data = register_form.data
-        name = data["name"]
+        first_name = data["first_name"]
+        last_name = data["last_name"]
+        address_1 = data["address_1"]
+        if data["address_2"]:
+            address_2 = data["address_2"]
+        else:
+            address_2 = None
+        postcode = data["postcode"]
+        city = data["city"]
+        country = data["country"]
+
         email = data["email"]
         password = data["password"]
         hashed_and_salted_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
         new_user = User(
-            name=name,
+            first_name=first_name,
+            last_name=last_name,
+            address_1=address_1,
+            address_2=address_2,
+            postcode=postcode,
+            city=city,
+            country=country,
             email=email,
             password=hashed_and_salted_password,
         )
@@ -99,3 +130,7 @@ def register():
             return redirect(url_for('get_all_posts'))
     return render_template("register.html", form=register_form, logged_in=current_user.is_authenticated,
                            user_id=current_user.get_id())
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
